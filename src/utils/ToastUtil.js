1809cdee6d17128d94f4f9e90567b795d82eabd3
kevin <agonyice0115@gmail.com>
2017-06-27 16:06:32 +0800

    add ToastUtils

--- src/utils/ToastUtil.js
+++ src/utils/ToastUtil.js
@@ -1,3 +1,28 @@
 /**
  * Created by kevin on 6/27/17.
  */
+import{Alert, ToastAndroid, Platform} from 'react-native';
+
+const showShortToast = (content, isAlter) => {
+  if (!isAlter) {
+    return
+  }
+  if (isAlter || Platform.OS === 'ios') {
+    Alert.alert("oops", content.toString());
+  } else {
+    ToastAndroid.show(content.toString(), ToastAndroid.SHORT);
+  }
+};
+
+const showLongToast = (content, isAlter) => {
+  if (isAlter || Platform.OS === 'ios') {
+    Alert.alert("oops", content.toString());
+  } else {
+    ToastAndroid.show(content.toString(), ToastAndroid.LONG);
+  }
+};
+
+export default {
+  showShortToast,
+  showLongToast
+};
